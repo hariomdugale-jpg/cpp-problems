e43ffad6280bb30081a8c73f881e8a2e6901fb95
hariom ugale <hariomdugale@gmail.com>
2026-02-27 23:38:33 +0530

    code for TO_PRINT_NO.FROM_1-10

---
+++ TO_PRINT_NO.FROM_1-10.cpp
@@ -0,0 +1,12 @@
+//Question 1:
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"The natural numbers are : ";
+    for(a=0;a<11;a=a+1)
+    {
+        cout<<a;
+
+    }
+}
